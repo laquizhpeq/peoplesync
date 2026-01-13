04c28b6a5bb2fa97f373bef75ab93395c77f78ce
serguiales <serguiales@gmail.com>
2026-01-13 13:13:07 +0100

    fix format dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,44 +1,38 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:peoplesync/main.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+
+class MockFirebasePlatform extends FirebasePlatform {
+  @override
+  FirebaseAppPlatform app([String name = defaultFirebaseAppName]) {
+    return FirebaseAppPlatform(
+      name,
+      const FirebaseOptions(
+        apiKey: 'test',
+        appId: 'test',
+        messagingSenderId: 'test',
+        projectId: 'test',
+      ),
+    );
+  }
+
+  @override
+  Future<FirebaseAppPlatform> initializeApp({
+    String? name,
+    FirebaseOptions? options,
+  }) async {
+    return app(name ?? defaultFirebaseAppName);
+  }
+
+  @override
+  List<FirebaseAppPlatform> get apps => [app()];
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUpAll(() async {
-    // Mock MethodChannel for Firebase Core
-    const MethodChannel channel = MethodChannel(
-      'plugins.flutter.io/firebase_core',
-    );
-
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
-          if (methodCall.method == 'Firebase#initializeCore') {
-            return [
-              {
-                'name': '[DEFAULT]',
-                'options': {
-                  'apiKey': 'test',
-                  'appId': 'test',
-                  'messagingSenderId': 'test',
-                  'projectId': 'test',
-                },
-                'pluginConstants': {},
-              },
-            ];
-          }
-          if (methodCall.method == 'Firebase#initializeApp') {
-            return {
-              'name': methodCall.arguments['appName'],
-              'options': methodCall.arguments['options'],
-              'pluginConstants': {},
-            };
-          }
-          return null;
-        });
-
-    await Firebase.initializeApp();
+  setUpAll(() {
+    FirebasePlatform.instance = MockFirebasePlatform();
   });
 
   testWidgets('Login screen smoke test', (WidgetTester tester) async {
