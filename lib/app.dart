b6a02d24882a329c796d9ae277710461e3519b07
laquizhpeq <luisangelmonike@gmail.com>
2026-01-14 13:46:31 +0100

    feature routes integration

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'core/constants/routes.dart';
 import 'features/auth/auth_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,7 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PeopleSync',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const AuthPage(),
+      initialRoute: Routes.login,
+      routes: {
+        Routes.login: (context) => const AuthPage(),
+        Routes.home: (context) => const AuthPage(),
+      },
     );
   }
 }
